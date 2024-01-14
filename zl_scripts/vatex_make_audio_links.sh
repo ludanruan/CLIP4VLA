1b0bc841de10d645831406bd2d5871fdb0c32414
ruanludan <ruanludan@baidu.com>
2024-01-14 18:23:08 +0800

    Update .gitignore

--- zl_scripts/vatex_make_audio_links.sh
+++
@@ -1,6 +0,0 @@
-python find_similar_audio.py \
-    --dir1 "/sharefs/multimodel/rld/CLIP4VLA_copy/ckpts/ckpt_vatex_video_retrieval/no_audio" \
-    --dir2 "/sharefs/multimodel/rld/CLIP4VLA_copy/ckpts/ckpt_vatex_video_retrieval/train_feat_audio" \
-    --output "/sharefs/multimodel/rld/data/vatex/audios_complement_16k" \
-    --audio_root "/sharefs/multimodel/rld/data/vatex/audios_16k"
-
